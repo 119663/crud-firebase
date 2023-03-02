d3513f16e79fded7e8328c46b57067d8c9ec11b2
119663 <evelyne.wambui@strathmore.edu#>
2023-03-02 10:45:42 +0300

    updates

--- src/components/signin.js
+++ src/components/signin.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './signin.css';
 import { useNavigate } from 'react-router-dom'
 import Goods from  './Goods.js';
+import Update from  './Update.js';
 import { useState } from "react";
 import { signInWithEmailAndPassword, signInWithPopup, signOut, updateCurrentUser} from 'firebase/auth';
 import{auth, google} from '../config/firebase-config';
@@ -16,7 +17,7 @@ const Signin = () => {
     const signIn = async () =>{
         try{
        await signInWithEmailAndPassword(auth,email,password);
-       <Goods/>
+       <Update/>
         } catch(err){
             console.error(err);
         }
